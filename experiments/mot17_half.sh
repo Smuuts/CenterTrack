db38957865b345cd814728805d3a650301412ea0
Simon Mathy <simon@halbig-mathy.de>
2022-05-20 12:20:02 +0200

    update

--- experiments/mot17_half.sh
+++ experiments/mot17_half.sh
@@ -1,6 +1,6 @@
 cd src
 # train
-python ../src/main.py tracking --exp_id mot17_half --dataset mot --dataset_version 17halftrain --pre_hm --ltrb_amodal --same_aug --hm_disturb 0.05 --lost_disturb 0.4 --fp_disturb 0.1 --gpus 0,1 --load_model ../models/crowdhuman.pth
+python ../src/main.py tracking --exp_id mot17_half --dataset mot --dataset_version 17halftrain --pre_hm --ltrb_amodal --same_aug --hm_disturb 0.05 --lost_disturb 0.4 --fp_disturb 0.1 --gpus 0 --load_model ../models/crowdhuman.pth
 # test
 python ../src/test.py tracking --exp_id mot17_half --dataset mot --dataset_version 17halfval --pre_hm --ltrb_amodal --track_thresh 0.4 --pre_thresh 0.5 --resume
 # test with public detection
