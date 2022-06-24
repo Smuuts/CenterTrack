044102a58358daf8c20e721d218d232d2ed408d6
Simon Mathy <simon@halbig-mathy.de>
2022-06-24 11:37:44 +0200

    fixed image ids

--- src/tools/convert_wildpark_to_coco.py
+++ src/tools/convert_wildpark_to_coco.py
@@ -5,18 +5,19 @@ DATA_PATH = '../../data/WildparkDataset/'
 OUT_PATH = DATA_PATH + 'annotations/'
 IMG_PATH = DATA_PATH + 'annotated_frames/'
 
-SPLITS = ['train', 'test', 'val']
+SPLITS = ['test', 'train', 'val']
 
 if __name__ == '__main__':
     data = AnimalDatasetImagecsv(IMG_PATH, DATA_PATH + 'pre_csv.csv', 'Wildpark', preprocessed=True)
     ann_cnt = 0
+    img_cnt = 0
     for split in SPLITS:
         print('Creating {} split.'.format(split))
         out_path = OUT_PATH + '{}.json'.format(split)
 
         out = {'images': [], 'annotations': [], 
-           'categories': [{'id': 1, 'name': 'red deer'}, {'id': 2, 'name': 'roe deer'}],
-           'videos': [{'id': 3, 'file_name': 'DW_2020_11_10_VD_00051'}, {'id': 8, 'file_name': 'RW_2020_11_10_VD_00151'}]}
+           'categories': [{'id': 1, 'name': 'red deer'}, {'id': 2, 'name': 'fallow deer'}],
+           'videos': [{'id': 3, 'file_name': 'DW_2020_11_10_VD_00051'}, {'id': 8, 'file_name': 'RW_2020_11_10_VD_00151'}, {'id': 19, 'file_name': 'DW_2020_12_02_VD_00315'}, {'id': 18, 'file_name': 'RW_2020_12_16_VD_00008'}, {'id': 20, 'file_name': 'DW_2020_12_16_VD_00003'}]}
            
         for i in range(len(data)):      # iterate through amount of videos
             imgs = data[i]
@@ -27,14 +28,13 @@ if __name__ == '__main__':
             
             for j in range(split_index * imgs_per_split, (split_index + 1) * imgs_per_split):   # give every split equal part of video
                 frame = data.getframe(imgs[j])[1]
-                # print(f'Converting {imgs[j]}')
-
                 img_id = int(frame['image_id'].item())
+                # print(f'{img_id}, img: {imgs[j]}')
                 image_info = {'file_name': imgs[j],
                       'id': img_id,
                       'frame_id': img_id,
-                      'prev_image_id': img_id - 1 if (img_id + 1)/(i+1) > 0 else -1,
-                      'next_image_id': (img_id + 1) if (img_id + 1)/(i+1) < video_length else -1,
+                      'prev_image_id': img_id - 1 if j != 0 else -1,
+                      'next_image_id': img_id + 1 if j != video_length-1 else -1,
                       'video_id': -1}
 
                 for video in out['videos']:
@@ -43,6 +43,8 @@ if __name__ == '__main__':
                         break
 
                 out['images'].append(image_info)
+
+                img_cnt = img_cnt + 1
 
                 for k in range(len(frame['boxes'])):
                     # turn bbox to coco format
@@ -56,7 +58,7 @@ if __name__ == '__main__':
                         'bbox': bbox,
                         'conf': 1.0}
 
-                    ann_cnt = ann_cnt + 1                    
+                    ann_cnt = ann_cnt + 1           
 
                     out['annotations'].append(ann)
 
